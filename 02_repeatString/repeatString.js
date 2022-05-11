d2f0923124c053e074c564d601039a98ad7f81c2
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-11 13:04:38 +0200

    add for loop
    it passes first tests, but fails on "returns ERROR with negative numbers"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    let result = "";
+    for (let i = num; i > 0; i--) {
+        result += string;
+    }
+    return result;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
