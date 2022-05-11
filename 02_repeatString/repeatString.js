497d1b37fcba10592abc8e7ee25813068649b9f9
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-11 13:34:16 +0200

    add condition for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
+
 function repeatString(string, num) {
     let result = "";
+    if (num < 0) {
+        result = "ERROR";
+    }
+
     for (let i = num; i > 0; i--) {
         result += string;
     }
