92db0f32b95c139c1e07cff9f989cd1d0a86bdf7
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-11 14:07:47 +0200

    slice, reverse and join string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let sliced = [];
+    let slicedReversed = [];
+    for (i = 0; i <= string.length; i++) {
+        sliced.push(string.slice(i, i + 1));
+    }
+    for (i = 0; i <= sliced.length; i++) {
+        slicedReversed.push(sliced[sliced.length - i]);
+    }
+    return slicedReversed.join("");
 
 };
 
