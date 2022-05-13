7b307638896d03006a7f73f6b861677dee4e4ea6
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-13 15:25:58 +0200

    add set of conditions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,16 +2,15 @@ const sumAll = function (startNum, finishNum) {
 
     //Gauss: (n / 2) (first number + last number) = sum
     //n= last-first+1
-    if (startNum < 0 || finishNum < 0) {
+    if (typeof startNum !== "number" || typeof finishNum !== "number") {
         return "ERROR";
-    }
-    if (startNum < finishNum) {
+    } else if (startNum < 0 || finishNum < 0) {
+        return "ERROR";
+    } else if (startNum < finishNum) {
         return ((finishNum - startNum + 1) / 2) * (startNum + finishNum);
     } else {
         return ((startNum - finishNum + 1) / 2) * (finishNum + startNum);
-
     }
-
 };
 
 // Do not edit below this line
