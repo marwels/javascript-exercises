296d91d05e52d9aa5c03903579e7214b9c4e33cd
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-13 13:28:18 +0200

    add array.filter with callback function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
-const removeFromArray = function (array, aremove, bremove, ...theArgs) {
+const removeFromArray = function (array, ...toRemove) {
 
-};
-
-
-
+    function isEqual(value) {
+        for (i = 0; i < toRemove.length; i++) {
+            if (value !== toRemove[i]) {
+                return true; // filter includes in new array those elements, for which is Equal returns true
+            }
+        }
+    }
+    let filtered = array.filter(isEqual);
+    return filtered;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
