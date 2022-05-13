b1deb39087b3a8a1c2f21faa6386617ebe8cbc76
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-13 12:16:56 +0200

    add function with rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, aremove, bremove, ...theArgs) {
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
