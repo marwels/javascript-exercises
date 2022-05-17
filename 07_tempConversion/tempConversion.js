06831b8dc057459b2fc6f8063e052e3801b3e381
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-17 14:02:22 +0200

    add conversion formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function () {
+const ftoc = function (fahrenheit) {
   //[°C] = ([°F] − 32) × 5⁄9
+  let celsius = (fahrenheit - 32) * 5 / 9;
 };
 
-const ctof = function () {
+const ctof = function (celsius) {
   //[°F] = [°C] × 9⁄5 + 32
+  let fahrenheit = celsius * 9 / 5 + 32;
+
 };
 
 // Do not edit below this line
