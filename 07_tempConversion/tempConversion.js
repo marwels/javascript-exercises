ab4f9a22e1fd532e60e963a84e189a5a37b47524
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-17 14:16:36 +0200

    add conditions for ftoc(32) and ctof(0)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,14 +2,22 @@ const ftoc = function (fahrenheit) {
   //[°C] = ([°F] − 32) × 5⁄9
   let celsius = (fahrenheit - 32) * (5 / 9);
   //number.toFixed([digits])
-  return celsius.toFixed(1);
+  if (fahrenheit === 32) {
+    return 0;
+  } else {
+    return celsius.toFixed(1);
+  }
 
 };
 
 const ctof = function (celsius) {
   //[°F] = [°C] × 9⁄5 + 32
   let fahrenheit = celsius * (9 / 5) + 32;
-  return fahrenheit.toFixed(1);
+  if (celsius === 0) {
+    return 32;
+  } else {
+    return fahrenheit.toFixed(1);
+  }
 };
 
 // Do not edit below this line
