f0f149e172667034c9eccbeb330240542f0389c9
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-17 14:08:59 +0200

    use number.toFixed([digits]) to round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,15 @@
 const ftoc = function (fahrenheit) {
   //[°C] = ([°F] − 32) × 5⁄9
-  let celsius = (fahrenheit - 32) * 5 / 9;
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  //number.toFixed([digits])
+  return celsius.toFixed(1);
+
 };
 
 const ctof = function (celsius) {
   //[°F] = [°C] × 9⁄5 + 32
-  let fahrenheit = celsius * 9 / 5 + 32;
-
+  let fahrenheit = celsius * (9 / 5) + 32;
+  return fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
