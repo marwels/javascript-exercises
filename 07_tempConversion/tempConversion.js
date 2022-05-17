6cc0ea03fb886cf072d506d2539e5d7c3c40a3a6
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-17 13:58:24 +0200

    add Fahrenheit to Celsius, and vice versa formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function () {
+  //[°C] = ([°F] − 32) × 5⁄9
 };
 
-const ctof = function() {
-
+const ctof = function () {
+  //[°F] = [°C] × 9⁄5 + 32
 };
 
 // Do not edit below this line
