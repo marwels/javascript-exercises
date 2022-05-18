99624e778f15666d4fb67a7ee3d449030869165d
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-18 16:11:47 +0200

    change string into array, add for loop

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (palin) {
+    let arrayPalin = palin.split("");
+    const filteredArrayPalin = arrayPalin.filter(Boolean);
+    //Falsy values include null, undefined, 0, empty string, NaN and false.
+    //So they’ll return false if we pass them into the Boolean function.
 
+    console.log(filteredArrayPalin);
+
+    for (i = 0; i < filteredArrayPalin.length; i++) {
+        if (filteredArrayPalin[i] === filteredArrayPalin[filteredArrayPalin.length - i]) {
+            continue;
+        } else { return false; }
+
+    }
 };
 
 // Do not edit below this line
