73cb1800b8f05b3c6505653c96968c88583372e1
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-18 14:26:15 +0200

    add sum function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (...toAdd) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
+
+  var sum = toAdd.reduce((a, b) => a + b, 0); // with initial value to avoid when the array is empty
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function () {
 
 };
 
-const power = function() {
-	
+const sum = function () {
+
 };
 
-const factorial = function() {
-	
+const multiply = function () {
+
+};
+
+const power = function () {
+
+};
+
+const factorial = function () {
+
 };
 
 // Do not edit below this line
