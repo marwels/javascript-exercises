7548051336dbc8b71a1cc930231058f122703796
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-19 14:27:36 +0200

    find largest age ang its place in arrAge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -7,13 +7,19 @@ const findTheOldest = function (arrayOfObjects) {
         arrAge.push(arrDeath[i] - arrBirth[i]);
     }
     console.log(arrAge);
+    let largest = Math.max.apply(arrAge);
+    console.log(largest);
+    //2. version of finding the largest:
+    //let temp = 0;
+    //arrAge.forEach((element) => {
+    //    if (temp < element) {
+    //       temp = element;
+    //    }
+    //});
 
-    let temp = 0;
-    arrAge.forEach((element) => {
-        if (temp < element) {
-            temp = element;
-        }
-    });
+    let searchedIndex = arrAge.indexOf(largest);
+    return arrayOfObjects[searchedIndex];
+
 };
 
 // Do not edit below this line
