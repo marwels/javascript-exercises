df5f131c6d8067e794abc47f97333cd498692226
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-19 12:18:03 +0200

    add function struction according to Fibonacci def

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let arrFibonacci = [1, 1];
+    for (i = 2; i <= num; i++) {
+        arrFibonacci.push(arrFibonacci[i - 1] + arrFibonacci[i - 2]);
+        console.log(arrFibonacci);
 
+    }
+    return arrFibonacci[num - 1];
 };
 
 // Do not edit below this line
