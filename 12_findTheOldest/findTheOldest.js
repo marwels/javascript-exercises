36a683cbf4ff353b9dd3161cc8afa3e79f02f409
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-19 14:09:31 +0200

    compute array of ages

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayOfObjects) {
+    let arrBirth = arrayOfObjects.map((object) => object.yearOfBirth);
+    let arrDeath = arrayOfObjects.map((object) => object.yearOfBirth);
+    let arrAge = [];
 
+    for (i = 0; i < arrayOfObjects.length; i++) {
+        arrAge.push(arrDeath[i] - arrBirth[i]);
+    }
+    console.log(arrAge);
+
+    let temp = 0;
+    arrAge.forEach((element) => {
+        if (temp < element) {
+            temp = element;
+        }
+    });
 };
 
 // Do not edit below this line
