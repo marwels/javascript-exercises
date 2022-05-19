8e7b752553573776932c1c7d1237c53971707b55
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-19 13:16:41 +0200

    add 3 versions of the answer

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function (arrayOfObjects) {
+    let titles = [];
+    for (i = 0; i < arrayOfObjects.length; i++) {
+        titles.push(arrayOfObjects[i].title);
+        console.log(titles);
+    }
 
+    //2. version return arrayOfObjects.map((object) => object.title).filter(Boolean)
+
+    //3. version for (const object of arrayOfObjects) {
+    //     if (object.title !== undefined) titles.push(object.title);
+    //}
+
+    return titles;
 };
 
 // Do not edit below this line
