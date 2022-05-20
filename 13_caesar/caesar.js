063d12f6bc485ad7265e1c60846347fdd93c02a6
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-20 14:07:02 +0200

    improove conditions by usig unicode values in them

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -2,17 +2,40 @@ const caesar = function (string, num) {
     let answer = "";
     for (i = 0; i < string.length; i++) {
         let sliced = string.slice(i, i + 1);
-        if (sliced === " " || sliced === "," || sliced === "." || sliced === "!" || sliced === "?") {
-            answer += sliced;
+        let slicedInUnicode = sliced.charCodeAt(0);
+
+        if (slicedInUnicode >= 0x0020 && slicedInUnicode <= 0x0040) {
+            answer += slicedInUnicode;
         } else {
             let inUnicode = string.charCodeAt(i);
-            let changed = String.fromCodePoint(inUnicode + num);
-            console.log("changed: " + changed);
-            answer += changed;
-            console.log("answer: " + answer);
+
+            if (inUnicode + num > 0x005A) {
+                answer += String.fromCodePoint(((inUnicode + num) - 0x005A) % 34); //34 alphabet letters
+            } else {
+                let changed = String.fromCodePoint(inUnicode + num);
+                console.log("changed: " + changed);
+                answer += changed;
+                console.log("answer: " + answer);
+            }
         }
     }
     return answer;
+
+
+    // version not basing on unicode numbers
+    // for (i = 0; i < string.length; i++) {
+    //     let sliced = string.slice(i, i + 1);
+    //     if (sliced === " " || sliced === "," || sliced === "." || sliced === "!" || sliced === "?") {
+    //         answer += sliced;
+    //     } else {
+    //         let inUnicode = string.charCodeAt(i);
+    //         let changed = String.fromCodePoint(inUnicode + num);
+    //        console.log("changed: " + changed);
+    //          answer += changed;
+    //         console.log("answer: " + answer);
+    //     }
+    //  }
+    //  return answer;
 };
 
 // Do not edit below this line
