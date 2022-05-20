bb71899da726104e96e89f7bbea4158cb8eb160d
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-20 11:22:34 +0200

    add basic structure of the function

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,10 @@
-const caesar = function() {
-
+const caesar = function (string) {
+    let answer = "";
+    for (i = 0; i < string.length; i++) {
+        answer += string.charCodeAt(i);
+        console.log(answer);
+    }
+    return answer;
 };
 
 // Do not edit below this line
